afd9df9c4479f4b33e55bf5d862111c96098bcbf
jettdlee <jettdlee@gmail.com>
2020-05-14 20:45:02 +0100

    build

---
+++ build/precache-manifest.a2e47330d7b58276bb91b531c12dcbf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63035b6cc76fccc5250e41a77d8da969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80a2ef00e922a5635bf",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2342c77c12b31c4ddd16",
+    "url": "/static/css/main.6bf38c91.chunk.css"
+  },
+  {
+    "revision": "e80a2ef00e922a5635bf",
+    "url": "/static/js/2.057cf661.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.057cf661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2342c77c12b31c4ddd16",
+    "url": "/static/js/main.bcc5fe03.chunk.js"
+  },
+  {
+    "revision": "6a80439d12d15cac70d1",
+    "url": "/static/js/runtime-main.5ddce5c8.js"
+  }
+]);
