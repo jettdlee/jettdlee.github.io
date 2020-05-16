27c5c694b8590e3a6eaa6091fa403518f8782856
jettdlee <jettdlee@gmail.com>
2020-05-16 16:49:56 +0100

    modals

--- src/components/portfolio/item.js
+++ src/components/portfolio/item.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { EllipsisVIcon } from '../../helpers/IconHelper';
+import { Image } from 'react-bootstrap';
+import PortfolioModal from './modal';
 
 function PortfolioItem(props) {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
+    <>
     <div className="col-md-6 col-lg-4 mb-5">
       <div className="portfolio-item" data-toggle="modal" data-target="#portfolioModal1">
-        <div className="portfolio-item-caption">
+        <div className="portfolio-item-caption" onClick={() => setModalShow(true)}>
           <div className="portfolio-item-caption-content">
             <div className="portfolio-item-text">
               {props.itemText}
@@ -18,9 +23,12 @@ function PortfolioItem(props) {
             <EllipsisVIcon extraClasses="fa-2x" />
           </div>
         </div>
-        <img className="portfolio-item-image" src="/images/portfolio/cabin.png" alt="" />
+        <Image className="portfolio-item-image" src="/images/portfolio/cabin.png" alt="" />
       </div>
     </div>
+
+    <PortfolioModal show={modalShow} onHide={() => setModalShow(false)} />
+    </>
   )
 }
 
--- src/components/portfolio/modal.js
+++ src/components/portfolio/modal.js
@@ -1,35 +1,31 @@
 import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 function PortfolioModal(props) {
   return (
-    <div className="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true">
-      <div className="modal-dialog modal-xl" role="document">
-        <div className="modal-content">
-          <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">
-              <i class="fas fa-times" />
-            </span>
-          </button>
-          <div className="modal-body text-center">
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-lg-8">
-                  <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
-                  <div className="divider-custom">
-                    <div className="divider-custom-line"></div>
-                    <div className="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div className="divider-custom-line"></div>
-                  </div>
-                  <img className="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="" />
-                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                  <button className="btn btn-primary" href="#" data-dismiss="modal"><i class="fas fa-times fa-fw"></i>Close Window</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
